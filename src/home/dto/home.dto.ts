925a082ba083c32dc47ba7220879395b7dd277cb
Dosunmu Oluwamuyiwa <oluwamuyiwadosunmu@gmail.com>
2023-08-17 11:35:01 +0100

    Implemented th GET homes controller and service attached

---
+++ src/home/dto/home.dto.ts
@@ -0,0 +1,63 @@
+import { PropertyType, UserType } from '@prisma/client';
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+  Min,
+  MinLength,
+} from 'class-validator';
+import { Exclude, Expose } from 'class-transformer';
+
+export class HomeResponseDto {
+  id: number;
+  address: string;
+
+  @Exclude()
+  number_of_bedrooms: number;
+
+  @Expose({ name: 'numberOfBedrooms' })
+  numberOfBedrooms() {
+    return this.number_of_bathrooms;
+  }
+
+  @Exclude()
+  number_of_bathrooms: number;
+  @Expose({ name: 'numberOfBathrooms' })
+  numberOfBathrooms() {
+    return this.number_of_bathrooms;
+  }
+
+  city: string;
+
+  @Exclude()
+  listed_date: Date;
+
+  @Expose({ name: 'listedDate' })
+  listedDate() {
+    return this.listed_date;
+  }
+  price: number;
+
+  @Exclude()
+  land_size: number;
+
+  @Expose({ name: 'landSize' })
+  landSize() {
+    return this.land_size;
+  }
+  propertyType: PropertyType;
+
+  @Exclude()
+  created_at: Date;
+  @Exclude()
+  updated_at: Date;
+  @Exclude()
+  realtor_id: number;
+
+  constructor(partial: Partial<HomeResponseDto>) {
+    Object.assign(this, partial);
+  }
+}
---
+++ src/home/home.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { HomeService } from './home.service';
+import { HomeResponseDto } from './dto/home.dto';
+
+@Controller('homes')
+export class HomeController {
+  constructor(private readonly homeService: HomeService) {}
+  @Get()
+  getHomes(): Promise<HomeResponseDto[]> {
+    return this.homeService.getHomes();
+  }
+
+  @Get(':id')
+  getHome() {
+    return {};
+  }
+
+  @Post()
+  createHome() {
+    return {};
+  }
+
+  @Put(':id')
+  updateHome() {
+    return {};
+  }
+
+  @Delete(':id')
+  deleteHome() {
+    return;
+  }
+}
